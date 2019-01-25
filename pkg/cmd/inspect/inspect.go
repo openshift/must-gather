84311f1bf4aa6edb25ecf243cf141b00692a5642
Rick Wagner <RickJWag@yahoo.com>
2019-01-25 13:10:24 -0600

    remove debug line

--- pkg/cmd/inspect/inspect.go
+++ pkg/cmd/inspect/inspect.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path"
-	"strconv"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -175,8 +174,6 @@ func (o *InspectOptions) Run() error {
 	if err := o.ensureDirectoryViable(o.baseDir, o.overwrite); err != nil {
 		return err
 	}
-
-	fmt.Println("GSSDefaults:" + strconv.FormatBool(o.gssdefaults))
 
 	// first, gather config.openshift.io resource data
 	errs := []error{}
