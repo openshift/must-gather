886e512a7a653829b8ec5327445bed363b48499f
Natalie Ammerman <nammerma@nammerma-na.rht.csb>
2025-06-02 11:17:29 -0400

    Rename

--- collection-scripts/monitoring_common.sh
+++ collection-scripts/monitoring_common.sh
@@ -37,7 +37,7 @@ metrics_get() {
     -c prometheus \
     -n openshift-monitoring \
     -- promtool tsdb dump-openmetrics /prometheus --sandbox-dir-root="/prometheus" "$@" \
-       > "${METRICS_PATH}/"${output_file}" \
+       > "${METRICS_PATH}/"${output_file}.openmetrics" \
        2> "${METRICS_PATH}/"${output_file}.stderr"
 }
 
